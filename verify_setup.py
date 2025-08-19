c449d551c4e017306d06e0795b5579df4bdce739
Ravi Bhargav <raavibhargav@outlook.com>
2025-08-19 16:08:05 +0530

    Add setup documentation and README

---
+++ verify_setup.py
@@ -0,0 +1,19 @@
+# verify_setup.py
+import sys
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
+import seaborn
+import scipy
+import statsmodels
+
+print("Python version:", sys.version)
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Seaborn version:", seaborn.__version__)
+print("SciPy version:", scipy.__version__)
+print("Statsmodels version:", statsmodels.__version__)
+
